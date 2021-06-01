f453349c31f7af0d0228a65b5fd5746abd7e1c6a
B0829009 <b0829009@cgu.edu.tw>
2021-06-01 21:47:39 +0800

    HW07

--- hello_django/settings.py
+++ hello_django/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -143,3 +144,7 @@ LOGGING = {
         },    
     },
 }
+
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
